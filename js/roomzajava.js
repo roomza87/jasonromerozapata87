d9bfd87cb1236f2bb7359cd06ce65ff94d001ba7
roomza87 <69545956+roomza87@users.noreply.github.com>
2020-08-11 18:00:34 -0500

    Add files via upload

---
+++ js/roomzajava.js
@@ -0,0 +1,6 @@
+// JavaScript Document
+var c='Bienvenidos a mi sección de Gif, todos los derechos están reservados //Jason Romero Zapata ';
+
+
+alert(c);
+
---
+++ js/roomzajava2.js
@@ -0,0 +1,5 @@
+// JavaScript Document
+var b='Sección de Videos //Jason Romero Zapata ';
+
+
+alert(b);
